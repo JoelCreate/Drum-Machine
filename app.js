56ddfaefb6f34edfcc8a292ab9cf83d29a2f9fb9
Joel Wiesner <j.wiesner88@gmail.com>
2023-05-02 18:08:42 -0700

    cleaned up JS

--- app.js
+++ app.js
@@ -1,64 +1,17 @@
-const yellow = document.getElementById("tap1")
-const orange = document.getElementById("tap2")
-const pink = document.getElementById("tap3")
-const blue = document.getElementById("tap4")
-const grey = document.getElementById("tap5")
-const purple = document.getElementById("tap6")
-const green = document.getElementById("tap7")
-const red = document.getElementById("tap8")
-const teal = document.getElementById("tap9")
-
-const button = document.querySelectorAll('.tap')
-button.forEach(djHit => djHit.addEventListener('transitionend', removeTransition))
+window.addEventListener("touchstart", function(e){    
+    const audio = document.querySelector(`audio[data-drum="${e.target.dataset.drum}"]`)
+    const tap = document.querySelector(`.tap[data-drum="${e.target.dataset.drum}"]`)
+    if (!audio) return // stop fuction all together
+    audio.currentTime = 0 //plays sound over and over
+    audio.play()
+    tap.classList.add('playing')
+})
 
 function removeTransition(e){
     if(e.propertyName !== 'transform') return
     this.classList.remove('playing')
 }
 
-yellow.addEventListener("touchstart", function (){
-    const djHit = new Audio("sounds/djHit.wav")  
-    djHit.play()
-    yellow.classList.add("playing")
-})
-
-orange.addEventListener("touchstart", function (){
-    const djScratch = new Audio("sounds/djScratch.wav")
-    djScratch.play()
-})
-
-pink.addEventListener("touchstart", function (){
-    const cymbal = new Audio("sounds/cymbal.wav")
-    cymbal.play()
-})
-
-blue.addEventListener("touchstart", function (){
-    const clap = new Audio("sounds/clap.wav")
-    clap.play()
-})
-
-grey.addEventListener("touchstart", function (){
-    const snare = new Audio("sounds/snare.wav")
-    snare.play()
-})
-
-purple.addEventListener("touchstart", function (){
-    const highTom = new Audio("sounds/highTom.wav")
-    highTom.play()
-})
-
-green.addEventListener("touchstart", function (){
-    const deepBass = new Audio("sounds/deepBass.wav")
-    deepBass.play()
-})
-
-red.addEventListener("touchstart", function (){
-    const danceBass = new Audio("sounds/danceBass.wav")
-    danceBass.play()
-})
-
-teal.addEventListener("touchstart", function (){
-    const lowTom = new Audio("sounds/lowTom.wav")
-    lowTom.play()
-})
+const taps = document.querySelectorAll('.tap')
+taps.forEach(tap => tap.addEventListener('transitionend', removeTransition))
 
