9e4a4b60c180c9cf742d964da0b6d17d707f6cfc
Joel Wiesner <j.wiesner88@gmail.com>
2023-05-03 19:04:39 -0700

    cleaned up JS

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-window.addEventListener("touchstart", function(e){    
+function playSound() {
     const audio = document.querySelector(`audio[data-drum="${e.target.dataset.drum}"]`)
     const tap = document.querySelector(`.tap[data-drum="${e.target.dataset.drum}"]`)
     if (!audio) return // stop fuction all together
     audio.currentTime = 0 //plays sound over and over
     audio.play()
     tap.classList.add('playing')
-})
+}
 
 function removeTransition(e){
     if(e.propertyName !== 'transform') return
@@ -13,5 +13,7 @@ function removeTransition(e){
 }
 
 const taps = document.querySelectorAll('.tap')
-taps.forEach(tap => tap.addEventListener('touchend', removeTransition))
+taps.forEach(tap => tap.addEventListener('trasitionend', removeTransition))
+
+window.addEventListener("touchstart", playSound)
 
